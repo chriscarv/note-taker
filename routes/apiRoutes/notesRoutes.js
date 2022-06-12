c80981250198acd00562cf35394295b991e028a8
christian <christiancarvajal@Christians-MacBook-Air.local>
2022-06-12 16:56:25 -0400

    get api route

---
+++ routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+router.get('./notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
