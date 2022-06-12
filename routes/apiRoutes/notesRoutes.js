36245b0efdb13f0e741e4023101b282320b3f706
christian <christiancarvajal@Christians-MacBook-Air.local>
2022-06-12 17:39:56 -0400

    api post route

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
+const {notes} = require('../../db/notes.json');
+const createNewNote = require('../../lib/notes');
 
 router.get('./notes', (req, res) => {
     let results = notes;
     res.json(results);
+});
+
+router.post('notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = createNewNote(req.body, notes);
+    res.json(note);
 })
 
 module.exports = router;
